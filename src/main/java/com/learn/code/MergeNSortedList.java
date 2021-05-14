5d18c7122c08967df06b5879d7281493e2e13f32
SuprasannaBhaumik_personal <suprasanna.bhaumik@daimler.com>
2021-05-14 21:48:26 +0530

    ListNode separated

---
+++ src/main/java/com/learn/code/MergeNSortedList.java
@@ -0,0 +1,15 @@
+package com.learn.code;
+
+public class MergeNSortedList {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        ListNode node = new ListNode();
+        return node;
+    }
+
+}
--- src/main/java/com/learn/code/MergeSortedList.java
+++ src/main/java/com/learn/code/MergeSortedList.java
@@ -74,11 +74,5 @@ public class MergeSortedList {
         return node;
     }
 
-    public static class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+
 }
